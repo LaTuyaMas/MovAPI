05977dd119077c02cd2e87065e5e6204aab6d3d9
Rapseo <103188330+Rapseo@users.noreply.github.com>
2023-04-04 10:21:43 +0200

    antes de actualizar angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import {GamesListComponent} from "./components/games-list/games-list.component";
-
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: GamesListComponent},
